66e61067d4dd97e11a3d81219c24fd050d07ab6c
Daniel Dyerly-145003729 <145003729@vusdnoc.vusd.k12.ca.us>
2017-11-01 10:23:23 -0700

    fixed loop

--- build/classes/cardsagainsthumanity/CardsAgainstHumanity.rs
+++
@@ -1,7 +0,0 @@
-cardsagainsthumanity.Asteroid
-cardsagainsthumanity.Window
-cardsagainsthumanity.CardsAgainstHumanity
-cardsagainsthumanity.Star
-cardsagainsthumanity.sound
-cardsagainsthumanity.Rocket
-cardsagainsthumanity.Missile
